a49e7330e38aa2e00ffb524869c809632f58b4ab
y.okuda <yutaro@Yutaros-MacBook-Air.local>
2025-03-22 13:42:37 +0800

    [Room Toolbar] ツールアイコン

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -1,3 +1,16 @@
+import {
+  Circle,
+  MousePointer2,
+  Pencil,
+  Redo2,
+  Square,
+  StickyNote,
+  Type,
+  Undo2,
+} from "lucide-react";
+
+import { ToolButton } from "./tool-button";
+
 export const Toolbar = () => {
   return (
     // top-[50%]: 縦半分の部分にtopの始まりを持ってくる
@@ -5,12 +18,27 @@ export const Toolbar = () => {
     // 要素が多くなるほど、始まりが上に移動する
     <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4">
       <div className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md">
-        <div>Pencil</div>
-        <div>Circle</div>
+        <ToolButton label="Select" icon={MousePointer2} onClick={() => {}} />
+        <ToolButton label="Text" icon={Type} onClick={() => {}} />
+        <ToolButton label="Sticky Note" icon={StickyNote} onClick={() => {}} />
+        <ToolButton label="Rectangle" icon={Square} onClick={() => {}} />
+        {/* Ellipseは楕円を意味する */}
+        <ToolButton label="Ellipse" icon={Circle} onClick={() => {}} />
+        <ToolButton label="Pen" icon={Pencil} onClick={() => {}} />
       </div>
       <div className="bg-white rounded-md p-1.5 flex flex-col items-center shadow-md">
-        <div>Undo</div>
-        <div>Redo</div>
+        <ToolButton
+          label="Undo"
+          icon={Undo2}
+          onClick={() => {}}
+          isDisabled={false}
+        />
+        <ToolButton
+          label="Redo"
+          icon={Redo2}
+          onClick={() => {}}
+          isDisabled={false}
+        />
       </div>
     </div>
   );
