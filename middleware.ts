160d5989ab769d4ea88ba15a6bcd109b8e353e31
y.okuda <yutaro@Yutaros-MacBook-Air.local>
2025-03-29 13:26:17 +0800

    [Development & Wrap-up] middleware.tsの変更

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,6 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)"]);
-
-export default clerkMiddleware(async (auth, request) => {
-  if (!isPublicRoute(request)) {
-    await auth.protect();
-  }
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
