a8377ab68906757b7fe3b0a2f3f52472b88aa765
y.okuda <yutaro@Yutaros-MacBook-Air.local>
2025-03-29 21:43:49 +0800

    [Development & Wrap-up] configにwebpackの設定を追加

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,10 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    config.cache = false; // eslint-disable-line
+    return config; // eslint-disable-line
+  },
 };
 
 export default nextConfig;
